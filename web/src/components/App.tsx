7769f8bc196b30f6d8949c55182fbf336ba7e818
Samuel Ko <koshunyin@gmail.com>
2025-01-01 22:19:14 +1100

    add logo

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Logo } from './Logo';
 
 export const App = () => {
-  return (<div> Hello, world!</div>);
+  return (
+    <div>
+      <Logo size={40} color="#7cb06d"/>
+      <p>Hello, world!</p>
+    </div>
+  );
 };
