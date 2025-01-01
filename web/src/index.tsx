6074d4504a642bccaac213e820d607a1f5e5c832
Samuel Ko <koshunyin@gmail.com>
2025-01-01 21:42:31 +1100

    edit code comment

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
 
-// create root div
+// create root container
 const container = document.createElement("div");
 container.id = "root";
 
-// add div to document body
+// insert container into document body
 document.body.appendChild(container);
 
-// load React components into div
+// render React components into container
 const root = ReactDOM.createRoot(container);
 root.render(<App />);
