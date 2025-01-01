93baa555307c96cb41bf46e282b3062c49c6a9dd
Samuel Ko <koshunyin@gmail.com>
2025-01-01 23:04:30 +1100

    inline the `Header` component as it would not be re-used

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Header } from './components/Header';
+import { Logo } from './components/Logo';
 
 export const App = () => {
   return (
     <>
-      <Header />
+      <header>
+        <Logo size={40} color='#ffffff' />
+      </header>
       <p>Hello, world!</p>
     </>
   );
