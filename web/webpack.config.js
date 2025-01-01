66c54d33d0a5e3a629e46441cc841b401c555571
Samuel Ko <koshunyin@gmail.com>
2025-01-01 20:45:40 +1100

    install `ts-loader`

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,6 +2,15 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
