7a279752617c939acef9d4cb3197fc5c227e629b
Samuel Ko <koshunyin@gmail.com>
2025-01-02 22:13:42 +1100

    add `/products` route

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import { LandingPage } from "./components/pages/LandingPage";
+import { ProductListingPage } from "./components/pages/ProductListingPage";
 
 const container = document.getElementById("root");
 if (!container) {
@@ -16,6 +17,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/products" element={<ProductListingPage />} />
     </Routes>
   </BrowserRouter>,
 );
