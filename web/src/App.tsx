dd28c58732bbaf32d9efb5896153784d09716a66
Samuel Ko <koshunyin@gmail.com>
2025-01-02 21:58:07 +1100

    set up `BrowserRouter`

--- web/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { LandingPage } from "./components/pages/LandingPage";
-
-export const App = () => {
-  return (
-    <LandingPage />
-  );
-};
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,8 +2,9 @@ import "./public/global.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-import { App } from "./App";
+import { LandingPage } from "./components/pages/LandingPage";
 
 const container = document.getElementById("root");
 if (!container) {
@@ -11,4 +12,10 @@ if (!container) {
 }
 
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+  </BrowserRouter>,
+);
