eb4c50941d6b910ab90d8cc2cd3a71fe4a5c6f31
Samuel Ko <koshunyin@gmail.com>
2025-01-02 23:21:35 +1100

    configure `vite build`

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,8 +2,12 @@ import type { UserConfig } from "vite";
 
 export default {
   root: "./src",
+  publicDir: "./src/assets",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
   },
-  publicDir: "./src/assets",
 } satisfies UserConfig;
