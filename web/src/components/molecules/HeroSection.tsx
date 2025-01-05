559f86089ac045fd8235368d2cb69c38f28225f4
Samuel Ko <koshunyin@gmail.com>
2025-01-05 15:18:32 +1100

    remove `ButtonLink`

--- web/src/components/molecules/HeroSection.tsx
+++ web/src/components/molecules/HeroSection.tsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 import heroImageUrl from "../../assets/hero-image.svg";
-import { ButtonLink } from "./ButtonLink";
+import { Button } from "../atoms/Button";
 import classes from "./HeroSection.module.css";
 
 export const HeroSection = () => {
+  const nagivate = useNavigate();
+
   return (
     <section className={classes.container}>
       <div>
         <h1>What are you waiting for?</h1>
-        <ButtonLink href="/products">Shop now</ButtonLink>
+        <Button onClick={() => nagivate("/products")}>Shop now</Button>
       </div>
       <img src={heroImageUrl} alt="image" />
     </section>
